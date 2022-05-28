50d160fb5cd2b1849a02fe64002399b0f741a7fe
Diki Rahmad Sandi <95979203+wawaiguntang@users.noreply.github.com>
2022-05-29 01:53:41 +0700

    Update database.js

--- app/config/database.js
+++ app/config/database.js
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize";
  
-const db = new Sequelize('adupi', 'root', '', {
-    host: "localhost",
+const db = new Sequelize('heroku_0571f51faddc42c', 'b7c30e98b6e0cc', 'c09deb71', {
+    host: "us-cdbr-east-05.cleardb.net",
     dialect: "mysql"
 });
  
