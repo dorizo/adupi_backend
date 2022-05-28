0e1818a9490982fdcb6746714ce3b13a00791788
dorizo <dorizo89@gmail.com>
2022-05-29 03:43:15 +0700

    Update index.js

--- index.js
+++ index.js
@@ -20,5 +20,8 @@ app.use(
 app.use(bodyParser.json());
 app.use(router);
 // app.use(expressValidator);
-
-app.listen(process.env.PORT || 5000 , () => console.log("Server running at port 5000"));
+var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
+app.listen(server_port, server_host, function() {
+    console.log('Listening on port %d', server_port);
+});
