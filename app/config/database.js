3143ff029f68d911e608fb1f28ad3175ff181a58
dorizo <dorizo89@gmail.com>
2022-05-29 15:44:45 +0700

    Update database.js

--- app/config/database.js
+++ app/config/database.js
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize";
  
-const db = new Sequelize('heroku_0571f51faddc42c', 'b7c30e98b6e0cc', 'c09deb71', {
-    host: "us-cdbr-east-05.cleardb.net",
+const db = new Sequelize('adupi', 'root', 'citm0029', {
+    host: "localhost",
     dialect: "mysql"
 });
  
