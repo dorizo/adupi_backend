3502f09a20fedd4e90e0aa4becff0e7a1b04169f
Diki Rahmad Sandi <95979203+wawaiguntang@users.noreply.github.com>
2022-05-31 01:54:08 +0700

    Update database.js

--- app/config/database.js
+++ app/config/database.js
@@ -6,7 +6,6 @@ import { Sequelize } from "sequelize";
 // });
 
 const db = new Sequelize('adupi', 'root', '', {
-const db = new Sequelize('adupi', 'root', 'citm0029', {
     host: "localhost",
     dialect: "mysql"
 });
