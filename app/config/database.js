a04a4788132941926ed6a70f301d6c92ecc2acea
Diki Rahmad Sandi <95979203+wawaiguntang@users.noreply.github.com>
2022-05-31 01:53:41 +0700

    Update database.js

--- app/config/database.js
+++ app/config/database.js
@@ -1,15 +1,12 @@
 import { Sequelize } from "sequelize";
- 
-<<<<<<< HEAD
+
 // const db = new Sequelize('heroku_0571f51faddc42c', 'b7c30e98b6e0cc', 'c09deb71', {
 //     host: "us-cdbr-east-05.cleardb.net",
 //     dialect: "mysql"
 // });
 
 const db = new Sequelize('adupi', 'root', '', {
-=======
 const db = new Sequelize('adupi', 'root', 'citm0029', {
->>>>>>> 0cbb0636846b2857953cfe8cea0366180441e022
     host: "localhost",
     dialect: "mysql"
 });
