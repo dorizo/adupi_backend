9ce51ee5324e661f90a2f019371530a04092c1c0
wawaiguntang <wawaiguntang@gmail.com>
2022-06-01 20:20:19 +0700

    config db for push

--- app/config/database.js
+++ app/config/database.js
@@ -1,11 +1,11 @@
 import { Sequelize } from "sequelize";
 
-// const db = new Sequelize('heroku_0571f51faddc42c', 'b7c30e98b6e0cc', 'c09deb71', {
-//     host: "us-cdbr-east-05.cleardb.net",
+// const db = new Sequelize('adupi', 'root', '', {
+//     host: "localhost",
 //     dialect: "mysql"
 // });
 
-const db = new Sequelize('adupi', 'root', '', {
+const db = new Sequelize('adupi', 'root', 'citm0029', {
     host: "localhost",
     dialect: "mysql"
 });
