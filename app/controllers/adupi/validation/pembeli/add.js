72ed79194cae576be94ac2bb99d1c7630c8bd1b2
Diki Rahmad Sandi <95979203+wawaiguntang@users.noreply.github.com>
2022-06-24 13:42:57 +0700

    Update add.js

--- app/controllers/adupi/validation/pembeli/add.js
+++ app/controllers/adupi/validation/pembeli/add.js
@@ -2,6 +2,6 @@ import { check } from "express-validator";
 import { model } from "../../../../models/index.js";
 
 export const addPembeliValidation = [
-  check("pembeli", "Pembeli tidak boleh kosong")
+  check("pembeliCode", "Pembeli tidak boleh kosong")
     .notEmpty(),
 ];
