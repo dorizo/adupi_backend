b70b67e3541704f3cd485fc5a5dc753c883fbd77
doris hermawan <dorizo89@gmail.com>
2022-08-29 02:28:10 +0700

    validasi ya

--- app/controllers/adupi/validation/kunjunganimage/add.js
+++ app/controllers/adupi/validation/kunjunganimage/add.js
@@ -1,7 +1,6 @@
 import { check } from "express-validator";
 
 export const addfotoValidationimage = [
-  check("image", "Judul tidak boleh kosong").notEmpty().trim().escape(),
   check("idku", "Deskripsi tidak boleh kosong").notEmpty().trim().escape(),
   check("statusfoto", "Deskripsi tidak boleh kosong").notEmpty().trim().escape(),
 ]
